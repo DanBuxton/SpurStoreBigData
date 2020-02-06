c721c90835bd168d7ccb6832464bb7c7d1e554c3
DanBuxton <dan.buxton99@gmail.com>
2020-02-06 21:22:43 +0000

    Add Supplier Class.

---
+++ SpurStoreBigData/Supplier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpurStoreBigData
+{
+    public class Supplier
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public Supplier(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0, 15s} : {1, -5}", Name, Type);
+        }
+    }
+}
