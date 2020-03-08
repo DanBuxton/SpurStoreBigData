0b27423bd7a867b841be758ec62106fd23da653f
DanBuxton <dan.buxton99@gmail.com>
2020-03-08 23:12:23 +0000

    Added a Graph that shows total cost of all orders for all time.

---
+++ SpurStoreBigData.GraphicalUserInterface/MainForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace SpurStoreBigData.GraphicalUserInterface
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            series3.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
+            series3.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(1123, 509);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Millimeter);
+            title3.Name = "Title1";
+            title3.Text = "Cost of all orders over time";
+            title3.TextStyle = System.Windows.Forms.DataVisualization.Charting.TextStyle.Shadow;
+            this.chart1.Titles.Add(title3);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1147, 533);
+            this.Controls.Add(this.chart1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
---
+++ SpurStoreBigData.GraphicalUserInterface/MainForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SpurStoreBigData.GraphicalUserInterface
+{
+    public partial class MainForm : Form
+    {
+        delegate void UpdateGraphDelegate();
+
+        private UpdateGraphDelegate updateGraphDelegate;
+
+        public Core Core { get; set; } = Core.Instance;
+
+        readonly List<string> x = new List<string>(); // Date
+        readonly List<double> y = new List<double>(); // Cost
+
+        public MainForm(Chart chart = null)
+        {
+            InitializeComponent();
+
+            if (chart == null)
+            {
+                chart1.Series["Series1"].LegendText = "Cost over time";
+            }
+            else
+            {
+                chart1 = chart;
+            }
+
+            chart1.Series["Series1"].Color = Color.Red;
+
+            updateGraphDelegate = new UpdateGraphDelegate(UpdateGraph);
+        }
+
+        private void Setup()
+        {
+            Core.FolderPath = @"C:\temp\Data";
+            Task.Factory.StartNew(() => Core.ReloadData(new System.Threading.CancellationTokenSource()))
+               .ContinueWith((t) =>
+               {
+                   var orders = Core.Orders.GroupBy(o => o.Date);
+
+                   var dates = Core.Dates;
+                   dates.Sort();
+
+                   foreach (Date date in dates)
+                   {
+                       x.Add(date.Week + "/" + (date.Year - 2000));
+                   }
+               })
+               .ContinueWith((t) =>
+               {
+                   Parallel.For(0, Core.Dates.Count, i =>
+                   {
+
+                       var date = Core.Dates[i];
+                    //var orders = Core.Orders.AsParallel().Where(o => o.Date.Equals(date));
+
+                    y.Add(Core.GetTotalCostOfAllOrdersInAWeek(date.Week, date.Year));
+                   });
+
+                //for (int j = 2013; j <= 2014; j++)
+                //{
+                //    Parallel.For(1, 53, new ParallelOptions() { MaxDegreeOfParallelism = 3 }, i =>
+                //    {
+                //        y.Add(Core.GetTotalCostOfAllOrdersInAWeek(i, j));
+                //    });
+
+                //    //for (int i = 1; i <= 52; i++)
+                //    //{
+                //    //y.Add(Core.GetTotalCostOfAllOrdersInAWeek(i, j));
+                //    //}
+                //}
+            })
+               .ContinueWith(t =>
+               {
+                   UpdateGraph();
+               });
+        }
+
+        private void UpdateGraph()
+        {
+            if (chart1.InvokeRequired)
+            {
+                chart1.Invoke(updateGraphDelegate);
+            }
+            else
+            {
+
+                chart1.Series["Series1"].Points.DataBindXY(x, y);
+                chart1.ChartAreas[0].AxisX.Interval = 4;
+                //chart1.ChartAreas[0].AxisX.IntervalType = DateTimeIntervalType.Weeks;
+                chart1.ChartAreas[0].AxisY.LabelStyle.Format = "C";
+                //chart1.Titles[0].Text = "";
+                //chart1.ChartAreas[0].AxisX.LabelStyle.Format = "dd/M/yyyy";
+            }
+        }
+    }
+}
